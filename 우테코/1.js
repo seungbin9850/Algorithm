2fbaaf22dd2cd3f022f3bf41db9197c65623caaf
yangseungbin <tanja1206@naver.com>
2020-11-21 22:15:58 +0900

    우테코 1번 짧은 풀이

--- 우테코/1.js
+++ 우테코/1.js
@@ -1,37 +1,20 @@
 function solution(grades, weights, threshold) {
-  let answer = 0;
-  for (let i = 0; i < grades.length; i++) {
-    switch (grades[i]) {
-      case "A+":
-        answer += 10 * weights[i];
-        break;
-      case "A0":
-        answer += 9 * weights[i];
-        break;
-      case "B+":
-        answer += 8 * weights[i];
-        break;
-      case "B0":
-        answer += 7 * weights[i];
-        break;
-      case "C+":
-        answer += 6 * weights[i];
-        break;
-      case "C0":
-        answer += 5 * weights[i];
-        break;
-      case "D+":
-        answer += 4 * weights[i];
-        break;
-      case "D0":
-        answer += 3 * weights[i];
-        break;
-      case "F":
-        answer += 0;
-        break;
-    }
-  }
-  return answer - threshold;
+  let a = {
+    "A+": 10,
+    A0: 9,
+    "B+": 8,
+    B0: 7,
+    "C+": 6,
+    C0: 5,
+    "D+": 4,
+    D0: 3,
+    F: 0,
+  };
+  return (
+    grades
+      .map((value, index) => a[value] * weights[index])
+      .reduce((acc, cur) => acc + cur) - threshold
+  );
 }
 
 console.log(solution(["A+", "D+", "F", "C0"], [2, 5, 10, 3], 50));
