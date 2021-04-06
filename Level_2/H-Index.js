1b573d74f95ec591b49f3fd8e362ce4d5f588f09
senugbin9850 <tanja1206@naver.com>
2021-04-06 19:03:24 +0900

    첫 번째 풀이

---
+++ Level_2/H-Index.js
@@ -0,0 +1,13 @@
+function solution(citations) {
+  let answer = 0;
+  citations.sort((a, b) => a - b);
+  console.log(citations);
+  for (let i = citations.length - 1; i >= 0; i--) {
+    let min =
+      citations[i] < citations.length - i ? citations[i] : citations.length - i;
+    if (answer < min) answer = min;
+  }
+  return answer;
+}
+
+console.log(solution([10, 9, 4, 1, 1]));
